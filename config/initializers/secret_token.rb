7aef0d46aed60751b22f2a386d0d944c7411083a
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-06-20 15:10:21 +0900

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZenjidoKanji::Application.config.secret_token = 'e6394a37fa14b29dfecf4bdbc293f8d0f321f7e9084d25e8e828b4df8e90f6035e3a46e7a94bbd61c8dc6671e981b40d9493a3973d2caf028d97cf4b4105a058'
