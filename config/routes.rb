2bc165233671b323192ff94b28e116e9b58eba92
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2012-06-21 23:32:28 +0900

    Generate user scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ZenjidoKanji::Application.routes.draw do
+  resources :users
+
   resources :events
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120621142642) do
+ActiveRecord::Schema.define(:version => 20120621143140) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(:version => 20120621142642) do
     t.date     "end_at"
     t.string   "place"
     t.integer  "budget"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "phone"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
